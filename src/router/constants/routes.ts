e8e18b3116498284d7f1e09701b85011178f2f88
codbear <douglas.meunier@openclassrooms.com>
2021-02-25 12:15:01 +0100

    feat(router): create router and add route for login

---
+++ src/router/constants/routes.ts
@@ -0,0 +1,7 @@
+const APP_ROUTES = {
+  USER: {
+    LOGIN: '/login',
+  },
+};
+
+export default APP_ROUTES;
---
+++ src/router/index.ts
@@ -0,0 +1,2 @@
+export { default as AppRouter } from './components/ReactRouter';
+export { default as APP_ROUTES } from './constants';
