1dd823b968cbee2080c526b85126d9084eed2b0d
codbear <douglas.meunier@codbear.com>
2021-05-13 18:02:13 +0200

    feat(auth): create LoginScreen

---
+++ modules/auth/screens/Login.tsx
@@ -0,0 +1,36 @@
+import { Container, makeStyles } from '@material-ui/core';
+
+import { LoginForm } from '../components/LoginForm';
+
+const useStyles = makeStyles({
+  screen: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  formContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+  },
+});
+
+const Login = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.screen}>
+      <Container className={classes.formContainer} maxWidth="sm">
+        <LoginForm />
+      </Container>
+    </div>
+  );
+};
+
+export default Login;
