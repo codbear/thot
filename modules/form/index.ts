8d71b30e4ba8da9f1ee6c327dd46d63dd350d397
codbear <douglas.meunier@codbear.com>
2021-05-22 18:12:53 +0200

    feat(form): create a formik field factory

--- modules/form/index.ts
+++ modules/form/index.ts
@@ -1,2 +1,4 @@
 export { FormikTextField } from './components/FormikTextField';
+export { FormikPasswordField } from './components/FormikPasswordField';
+export { FormikField } from './components/FormikField';
 export { isEmail, isRequired } from './services';
