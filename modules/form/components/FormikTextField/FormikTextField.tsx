51ad1699b6cbb5535713c7bb150bb3d88bafd0a8
codbear <douglas.meunier@codbear.com>
2021-05-22 18:11:41 +0200

    refactor(form): create a common TextField component

--- modules/form/components/FormikTextField/FormikTextField.tsx
+++ modules/form/components/FormikTextField/FormikTextField.tsx
@@ -1,41 +1,20 @@
 import { ReactNode } from 'react';
 import { Field, FieldProps } from 'formik';
 
-import { FormHelperText, makeStyles, TextField as MaterialTextField } from '@material-ui/core';
-
 import { validateField, Validator } from '../../services';
+
+import { TextField } from '../TextField';
 
 type PropTypes = {
   name: string;
   validators?: Validator[];
-  noAsterisk?: boolean;
-  inputProps?: {};
 };
 
 const defaultProps = {
   validators: [],
-  noAsterisk: false,
-  inputProps: {},
 };
 
-const useStyles = makeStyles(({ spacing }) => ({
-  root: {
-    marginBottom: spacing(2),
-  },
-  noAsterisk: {
-    display: 'none',
-  },
-}));
-
-const FormikTextField = ({
-  name,
-  validators,
-  noAsterisk,
-  inputProps,
-  ...otherProps
-}: PropTypes) => {
-  const classes = useStyles();
-
+const FormikTextField = ({ name, validators, ...otherProps }: PropTypes) => {
   return (
     <Field name={name} validate={validateField(validators)}>
       {(formikProps: FieldProps): ReactNode => {
@@ -43,27 +22,7 @@ const FormikTextField = ({
         const error = form.touched[field.name] && form.errors[field.name];
 
         return (
-          <div className={classes.root}>
-            <MaterialTextField
-              id={`field${field.name}`}
-              error={!!error}
-              variant="outlined"
-              InputLabelProps={{
-                classes: {
-                  ...(noAsterisk ? { asterisk: classes.noAsterisk } : {}),
-                },
-              }}
-              inputProps={inputProps}
-              {...field}
-              {...formikProps}
-              {...otherProps}
-            />
-            {error && (
-              <FormHelperText id={`${field.name}error`} error>
-                {error}
-              </FormHelperText>
-            )}
-          </div>
+          <TextField field={field} error={error} {...field} {...formikProps} {...otherProps} />
         );
       }}
     </Field>
